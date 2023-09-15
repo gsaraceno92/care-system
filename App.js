0556053084244ee90d619f331fe2b7010cd82af2
giuseppe.saraceno <giuseppe.saraceno@cambiomarcia.it>
2023-09-15 17:46:34 +0200

    feat: web start, first component

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
+import Hello from "components/Hello";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App to start working on your app!</Text>
+      <Hello></Hello>
       <StatusBar style="auto" />
     </View>
   );
