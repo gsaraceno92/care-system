54b78bf4a92a61aeffa18942cb5fb84a2c53cff4
giuseppe.saraceno <giuseppe.saraceno@cambiomarcia.it>
2023-09-20 14:57:02 +0200

    feat: react-native cli

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,19 @@
 module.exports = function (api) {
-  api.cache(true);
+  // api.cache(true);
   return {
     // presets: ["babel-preset-expo"],
-    presets: ["module:metro-react-native-babel-preset"],
+    presets: ['module:metro-react-native-babel-preset'],
     plugins: [
       [
-        "module-resolver",
+        'module-resolver',
         {
-          root: ["./src"],
-          extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
           alias: {
-            tests: ["./tests/"],
-            "@components": "./src/components",
+            tests: ['./tests/'],
+            '@components': './src/components',
           },
         },
-        "nativewind/babel",
       ],
     ],
   };
